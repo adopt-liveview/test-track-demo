ad14a56643f36ea370b7938c7151f8af979553d8
lubien <lubien1996@gmail.com>
2024-04-10 03:54:58 -0300

    live-component-done

--- lib/super_store_web/live/product_live/form_component.ex
+++ lib/super_store_web/live/product_live/form_component.ex
@@ -1,14 +1,69 @@
 defmodule SuperStoreWeb.ProductLive.FormComponent do
-  use SuperStoreWeb, :html
+  use SuperStoreWeb, :live_component
+  alias SuperStore.Catalog
+  alias SuperStore.Catalog.Product
 
-  attr :form, Phoenix.HTML.Form
-  attr :rest, :global, include: ~w(phx-change phx-submit)
-  slot :inner_block
+  def update(%{product: product} = assigns, socket) do
+    form =
+      Product.changeset(product)
+      |> to_form()
+
+    {:ok,
+     socket
+     |> assign(form: form)
+     |> assign(assigns)}
+  end
+
+  def handle_event("validate", %{"product" => product_params}, socket) do
+    form =
+      socket.assigns.product
+      |> Product.changeset(product_params)
+      |> Map.put(:action, :validate)
+      |> to_form()
+
+    {:noreply, assign(socket, form: form)}
+  end
+
+  def handle_event("save", %{"product" => product_params}, socket) do
+    case socket.assigns.action do
+      :new ->
+        case Catalog.create_product(product_params) do
+          {:ok, product} ->
+            {:noreply,
+             socket
+             |> put_flash(:info, "Product created successfully")
+             |> push_navigate(to: ~p"/")}
+
+          {:error, %Ecto.Changeset{} = changeset} ->
+            form = to_form(changeset)
+            {:noreply, assign(socket, form: form)}
+        end
+
+      :edit ->
+        case Catalog.update_product(socket.assigns.product, product_params) do
+          {:ok, product} ->
+            {:noreply,
+             socket
+             |> put_flash(:info, "Product updated successfully")
+             |> push_navigate(to: ~p"/products/#{product.id}/edit")}
+
+          {:error, %Ecto.Changeset{} = changeset} ->
+            form = to_form(changeset)
+            {:noreply, assign(socket, form: form)}
+        end
+    end
+  end
 
   def render(assigns) do
     ~H"""
     <div class="bg-grey-100">
-      <.form for={@form} class="flex flex-col max-w-96 mx-auto bg-gray-100 p-24" {@rest}>
+      <.form
+        for={@form}
+        phx-target={@myself}
+        phx-change="validate"
+        phx-submit="save"
+        class="flex flex-col max-w-96 mx-auto bg-gray-100 p-24"
+      >
         <%= render_slot(@inner_block) %>
         <.input field={@form[:name]} placeholder="Name" />
         <.input field={@form[:description]} placeholder="Description" />
--- lib/super_store_web/live/product_live/new.ex
+++ lib/super_store_web/live/product_live/new.ex
@@ -1,43 +1,8 @@
 defmodule SuperStoreWeb.ProductLive.New do
   use SuperStoreWeb, :live_view
   import SuperStoreWeb.CoreComponents
-  alias SuperStore.Catalog
   alias SuperStore.Catalog.Product
   alias SuperStoreWeb.ProductLive.FormComponent
-
-  def mount(_params, _session, socket) do
-    form =
-      Product.changeset(%Product{})
-      |> to_form()
-
-    {:ok, assign(socket, form: form)}
-  end
-
-  def handle_event("validate_product", %{"product" => product_params}, socket) do
-    form =
-      Product.changeset(%Product{}, product_params)
-      |> Map.put(:action, :validate)
-      |> to_form()
-
-    {:noreply, assign(socket, form: form)}
-  end
-
-  def handle_event("create_product", %{"product" => product_params}, socket) do
-    socket =
-      case Catalog.create_product(product_params) do
-        {:ok, %Product{} = product} ->
-          put_flash(socket, :info, "Product ID #{product.id} created!")
-
-        {:error, %Ecto.Changeset{} = changeset} ->
-          form = to_form(changeset)
-
-          socket
-          |> assign(form: form)
-          |> put_flash(:error, "Invalid product!")
-      end
-
-    {:noreply, socket}
-  end
 
   def render(assigns) do
     ~H"""
@@ -46,9 +11,9 @@ defmodule SuperStoreWeb.ProductLive.New do
       <:subtitle>Use this form to create product records in your database.</:subtitle>
     </.header>
 
-    <FormComponent.render form={@form} phx-change="validate_product" phx-submit="create_product">
+    <.live_component module={FormComponent} id="new-form" product={%Product{}} action={@live_action}>
       <h1>Creating a product</h1>
-    </FormComponent.render>
+    </.live_component>
 
     <.back navigate={~p"/"}>Back to products</.back>
     """
